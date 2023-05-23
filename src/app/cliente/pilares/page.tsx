9c85770d69017fe2ede448407c312f3364e4774e
raphauy <rapha.uy@rapha.uy>
2023-05-23 16:26:39 -0300

    version_2

--- src/app/cliente/pilares/page.tsx
+++ src/app/cliente/pilares/page.tsx
@@ -7,10 +7,10 @@ import { fill } from '@cloudinary/url-gen/actions/resize';
 export default function PilaresPage() {
   const myImage = new CloudinaryImage("https://res.cloudinary.com/dtm41dmrz/image/upload/v1684609584/tinta-posts/lyy7zl7r94hmceouli3p.jpg".split("/").slice(-2).join("/"), {cloudName: 'dtm41dmrz'}).resize(fill().width(400).height(400));
   return (
-    <div>
-      
-      <p>PilaresPage</p>
-      <AdvancedImage cldImg={myImage} />
+    <div className='flex items-center justify-center min-h-[300px] text-5xl font-bold'>      
+
+      <p>Comming soon</p>
+
     </div>
   )
 }
