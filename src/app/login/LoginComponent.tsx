a2c49f1ee9491e333d31d3e5e30cc7a115a0d5dd
raphauy <rapha.uy@rapha.uy>
2023-05-23 16:15:55 -0300

    version_2

--- src/app/login/LoginComponent.tsx
+++ src/app/login/LoginComponent.tsx
@@ -5,22 +5,48 @@ import Image from "next/image";
 import Avatar from "react-avatar";
 import PopOver from "../../components/modal/PopOver";
 import PopOverUserHandler from "./PopOverUserHandler";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { AdvancedImage } from "@cloudinary/react";
+import Client from "../types/Client";
+import { CloudinaryImage } from "@cloudinary/url-gen";
+import LoadingSpinner from "@/components/LoadingSpinner";
 
 export default function LoginComponent() {
+    const [client, setClient] = useState<Client>();
+    
+    useEffect(() => {
+      async function fetchClient() {
+        const { data } = await axios.get(`/api/client`);
+        return data.data;
+      }
+    
+      fetchClient()
+      .then((res) => setClient(res));
+    }, []);
 
     const { data:session }= useSession()
-
     const user= session?.user
 
-    if (!user)
-        return <div></div>
+    if (!user) return <LoadingSpinner />
+    if (!client) return <LoadingSpinner />
+
+    const avatarImage = new CloudinaryImage(client.image_insta.split("/").slice(-2).join("/"), {cloudName: 'dtm41dmrz'})
 
     const avatar= (
         <div>
             {user?.image ?             
             <Image className="rounded-full w-14" src={user?.image} width={116} height={35} alt="logo" /> : 
-            <Avatar name={user?.email || ""} round={true} size="50" color="#AF8928" className="font-bold cursor-pointer hover:opacity-80"/>}
+            <>
+            { user.role === "agency" ?
+            <Avatar name={user?.email || ""} round={true} size="50" color="#AF8928" className="font-bold cursor-pointer hover:opacity-80"/> :
+            <div className="relative inline-block w-8 h-8 overflow-hidden border rounded-full md:h-14 md:w-14">
+                <AdvancedImage cldImg={avatarImage} />
+            </div>
+            }
             <span className="absolute block w-2 h-2 bg-green-500 rounded-full right-8 top-4 ring-2 ring-white md:h-3 md:w-3"></span>
+        </>        
+        }
         </div>
     )
 
