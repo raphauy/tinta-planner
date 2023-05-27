187c832bee651c4790e394d86fd924b476fa6a3d
raphauy <rapha.uy@rapha.uy>
2023-05-27 09:18:27 -0300

    client view, users page added

--- src/app/cliente/usuarios/page.tsx
+++ src/app/cliente/usuarios/page.tsx
@@ -1,7 +1,35 @@
+import { getClientOfCurrenUser } from '@/app/(server-side)/services/getClients';
 import CommingSoon from '@/components/CommingSoon'
 
-export default function UsuariosPage() {
+export default async function UsuariosPage() {
+  const client= await getClientOfCurrenUser()
+  if (!client) return <div>Client not found</div>
+
+  const users= client.users
+  console.log("users: " + users.length);
+
   return (
-    <CommingSoon />
+    <div className="p-5 mx-auto md:w-2/3">
+      <div className="p-5 bg-white border rounded-md">
+        <table className="w-full">
+          <thead>
+            <tr className="h-12 font-medium text-left text-gray-700 align-middle bg-gray-100 brder-b text-muted-foreground">
+              <th className="pl-3">Nombre</th>
+              <th className="">Email</th>
+            </tr>
+          </thead>
+          <tbody>
+          {
+          users.map((user) => (
+            <tr key={user.id} className="h-12 px-4 font-medium text-left align-middle border-b text-muted-foreground hover:bg-slate-100">
+                <td className="pl-3 text-gray-600">{user.name}</td>
+                <td className="text-gray-600">{user.email}</td>
+            </tr>
+          ))
+          }
+          </tbody>
+        </table>
+      </div>
+  </div>
   )
 }
