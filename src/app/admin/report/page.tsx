797deafb8f7dfb0ea15f41a3b68e5240df373622
raphauy <rapha.uy@rapha.uy>
2023-05-30 09:28:38 -0300

    version_2.4 Post carousel added

--- src/app/admin/report/page.tsx
+++ src/app/admin/report/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import CommingSoon from '@/components/CommingSoon';
 import PostCarouselForm from '../[slug]/posts/PostCarouselForm'
 import { useState } from 'react';
 
@@ -8,36 +9,6 @@ export default function ReportPage() {
   const [images, setImages] = useState(fakeImages);
 
   return (
-  <main className="w-full h-full md:flex">
-
-    <div className="flex-grow w-full h-full ">            
-
-      <div className='flex flex-col justify-center h-full w-96'>
-        <div className='w-full h-full p-4 bg-white border'>
-          <PostCarouselForm images={images} />
-        </div>
-        <button
-          onClick={() => {
-            const newImage = "tinta-posts/nge2dn8asukrjdnzb8gl.jpg"
-            setImages((prevImages) => [...prevImages,newImage])
-          }}
-        >
-          Add
-        </button>
-        <button
-          onClick={() => {
-            const newImage = "tinta-posts/kozrv6aucphrxepaw2mm.jpg"
-            setImages((prevImages) => [...prevImages,newImage])
-          }}
-        >
-          Add
-        </button>
-
-      </div>    
-
-    </div>    
-
-  </main>
-
+    <CommingSoon />
   )
 }
