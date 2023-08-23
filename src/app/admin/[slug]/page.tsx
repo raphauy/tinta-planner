a193628f1d1dcd5d58cdb3ca4a21ca5f0e9bcafa
raphauy <rapha.uy@rapha.uy>
2023-08-23 16:42:07 -0300

    wines to client view

--- src/app/admin/[slug]/page.tsx
+++ src/app/admin/[slug]/page.tsx
@@ -1,7 +1,9 @@
 import { formatter } from "@/app/(client-side)/utils";
 import { getClientBySlug } from "@/app/(server-side)/services/getClients";
 import getCurrentUser from "@/app/(server-side)/services/getCurrentUser";
+import getClientWines from "@/app/(server-side)/services/getWines";
 import LoadingSpinner from "@/components/LoadingSpinner"
+import { Wine } from "lucide-react";
 import { headers } from "next/dist/client/components/headers";
 import Link from "next/link";
 import { AiOutlineInstagram } from "react-icons/ai";
@@ -14,52 +16,55 @@ export default async function DashboardPage({ params }: { params: { slug: string
   const { slug }= params  
 
   const client = await getClientBySlug(slug)
+  if (!client) return <div>Client not found</div>
 
   const user= await getCurrentUser()
+
+  const wines= await getClientWines(client.id)
 
   console.log("[" + formatter.format(new Date()) + "] " + user?.name + " (admin)");
   
   if (!client) return <LoadingSpinner />
 
     return (
-      <>
-      <section className="w-full text-gray-600 body-font">
-        <div className="container px-5 mx-auto py-14">
-          <div className="flex flex-col w-full text-center mb-14">
-            <h1 className="mb-4 text-2xl font-medium text-gray-900 sm:text-4xl title-font">{client.name}</h1>
-          </div>
-          <div className="flex flex-wrap -m-4 text-center">
-            <div className="w-full p-4 md:w-1/3 sm:w-1/2">
-              <Link href={`/admin/${slug}/posts`}>
-                <div className="flex flex-col items-center px-4 py-6 border-2 border-gray-200 rounded-lg">
-                  <AiOutlineInstagram size={65} className="text-tinta-vino"/>
-                  <h2 className="text-3xl font-medium text-gray-900 title-font">{client.posts.length}</h2>
-                  <p className="leading-relaxed">Posts</p>
-                </div>
-              </Link>
-            </div>
-            <div className="w-full p-4 md:w-1/3 sm:w-1/2">
-            <Link href={`/admin/${slug}/pilares`}>
-              <div className="flex flex-col items-center px-4 py-6 border-2 border-gray-200 rounded-lg">
-                <BsHddStack size={65} className="text-tinta-vino"/>                
-                <h2 className="text-3xl font-medium text-gray-900 title-font">{client.pilars.length}</h2>
-                <p className="leading-relaxed">Pilares de contenido</p>
-              </div>
-              </Link>
-            </div>
-            <div className="w-full p-4 md:w-1/3 sm:w-1/2">
-              <Link href={`/admin/${slug}/usuarios`}>
-                <div className="flex flex-col items-center px-4 py-6 border-2 border-gray-200 rounded-lg">
-                  <FaRegUserCircle size={65} className="text-tinta-vino"/>
-                  <h2 className="text-3xl font-medium text-gray-900 title-font">{client.users.length}</h2>
-                  <p className="leading-relaxed">Usuarios</p>
-                </div>
-              </Link>
-            </div>
-          </div>
+      <div className="w-full">
+        <div className="flex flex-col w-full text-center py-14">
+          <h1 className="mb-4 text-2xl font-medium text-gray-900 sm:text-4xl title-font">{client.name}</h1>
         </div>
-      </section>
-    </>
+        
+        <section className="grid w-full grid-cols-1 gap-8 px-5 mx-auto text-gray-600 md:grid-cols-2 lg:grid-cols-3 body-font">
+          <Link href={`/admin/${slug}/posts`}>
+            <div className="flex flex-col items-center px-4 py-6 border-2 border-gray-200 rounded-lg">
+              <AiOutlineInstagram size={65} className="text-tinta-vino"/>
+              <h2 className="text-3xl font-medium text-gray-900 title-font">{client.posts.length}</h2>
+              <p className="leading-relaxed">Posts</p>
+            </div>
+          </Link>
+          <Link href={`/admin/${slug}/pilares`}>
+            <div className="flex flex-col items-center px-4 py-6 border-2 border-gray-200 rounded-lg">
+              <BsHddStack size={65} className="text-tinta-vino"/>                
+              <h2 className="text-3xl font-medium text-gray-900 title-font">{client.pilars.length}</h2>
+              <p className="leading-relaxed">Pilares de contenido</p>
+            </div>
+          </Link>
+          <Link href={`/admin/${slug}/usuarios`}>
+            <div className="flex flex-col items-center px-4 py-6 border-2 border-gray-200 rounded-lg">
+              <FaRegUserCircle size={65} className="text-tinta-vino"/>
+              <h2 className="text-3xl font-medium text-gray-900 title-font">{client.users.length}</h2>
+              <p className="leading-relaxed">Usuarios</p>
+            </div>
+          </Link>
+          <div className="hidden lg:block"></div>
+          <Link href={`/admin/${slug}/wines`}>
+            <div className="flex flex-col items-center px-4 py-6 border-2 border-gray-200 rounded-lg">
+              <Wine size={65} className="text-tinta-vino"/>
+              <h2 className="text-3xl font-medium text-gray-900 title-font">{wines.length}</h2>
+              <p className="leading-relaxed">Vinos</p>
+            </div>
+          </Link>
+        </section>
+
+      </div>
     )
   }
   
--- src/app/cliente/page.tsx
+++ src/app/cliente/page.tsx
@@ -7,56 +7,59 @@ import { BsHddStack } from "react-icons/bs";
 import { FaRegUserCircle } from "react-icons/fa";
 import getCurrentUser from "../(server-side)/services/getCurrentUser";
 import { formatter } from "../(client-side)/utils";
+import getClientWines from "../(server-side)/services/getWines";
+import { Wine } from "lucide-react";
 
 export default async function ClientPage() {
   const client = await getClientOfCurrenUser();
 
-  if (!client) return <LoadingSpinner />
+  if (!client) return <div>Cliente no encontrado. Por favor haga refresh o login nuevamente</div>
 
   const user= await getCurrentUser()
+
+  const wines= await getClientWines(client.id)
 
   console.log("[" + formatter.format(new Date()) + "] " + user?.name + " ("+ client.name + ")");
 
   return (
-    <>
-      <section className="text-gray-600 body-font">
-        <div className="container px-5 mx-auto py-14">
-          <div className="flex flex-col w-full text-center mb-14">
-            <h1 className="mb-4 text-2xl font-medium text-gray-900 sm:text-4xl title-font">{client.name}</h1>
-            <p className="mx-auto text-base leading-relaxed lg:w-2/3">{client.description}</p>
+    <div className="w-full">
+      <div className="flex flex-col w-full text-center py-14">
+        <h1 className="mb-4 text-2xl font-medium text-gray-900 sm:text-4xl title-font">{client.name}</h1>
+      </div>
+      
+      <section className="grid w-full grid-cols-1 gap-8 px-5 mx-auto text-gray-600 md:grid-cols-2 lg:grid-cols-3 body-font">
+        <Link href={`/cliente/posts`}>
+          <div className="flex flex-col items-center px-4 py-6 border-2 border-gray-200 rounded-lg">
+            <AiOutlineInstagram size={65} className="text-tinta-vino"/>
+            <h2 className="text-3xl font-medium text-gray-900 title-font">{client.posts.length}</h2>
+            <p className="leading-relaxed">Posts</p>
           </div>
-          <div className="flex flex-wrap -m-4 text-center">
-            <div className="w-full p-4 md:w-1/3 sm:w-1/2">
-              <Link href="/cliente/posts">
-                <div className="flex flex-col items-center px-4 py-6 border-2 border-gray-200 rounded-lg">
-                  <AiOutlineInstagram size={65} className="text-tinta-vino"/>
-                  <h2 className="text-3xl font-medium text-gray-900 title-font">{client.posts.length}</h2>
-                  <p className="leading-relaxed">Posts</p>
-                </div>
-              </Link>
-            </div>
-            <div className="w-full p-4 md:w-1/3 sm:w-1/2">
-            <Link href="/cliente/pilares">
-              <div className="flex flex-col items-center px-4 py-6 border-2 border-gray-200 rounded-lg">
-                <BsHddStack size={65} className="text-tinta-vino"/>                
-                <h2 className="text-3xl font-medium text-gray-900 title-font">{client.pilars.length}</h2>
-                <p className="leading-relaxed">Pilares de contenido</p>
-              </div>
-              </Link>
-            </div>
-            <div className="w-full p-4 md:w-1/3 sm:w-1/2">
-              <Link href="/cliente/usuarios">
-                <div className="flex flex-col items-center px-4 py-6 border-2 border-gray-200 rounded-lg">
-                  <FaRegUserCircle size={65} className="text-tinta-vino"/>
-                  <h2 className="text-3xl font-medium text-gray-900 title-font">{client.users.length}</h2>
-                  <p className="leading-relaxed">Usuarios</p>
-                </div>
-              </Link>
-            </div>
+        </Link>
+        <Link href={`/cliente/pilares`}>
+          <div className="flex flex-col items-center px-4 py-6 border-2 border-gray-200 rounded-lg">
+            <BsHddStack size={65} className="text-tinta-vino"/>                
+            <h2 className="text-3xl font-medium text-gray-900 title-font">{client.pilars.length}</h2>
+            <p className="leading-relaxed">Pilares de contenido</p>
           </div>
-        </div>
+        </Link>
+        <Link href={`/cliente/usuarios`}>
+          <div className="flex flex-col items-center px-4 py-6 border-2 border-gray-200 rounded-lg">
+            <FaRegUserCircle size={65} className="text-tinta-vino"/>
+            <h2 className="text-3xl font-medium text-gray-900 title-font">{client.users.length}</h2>
+            <p className="leading-relaxed">Usuarios</p>
+          </div>
+        </Link>
+        <div className="hidden lg:block"></div>
+        <Link href={`/cliente/vinos`}>
+          <div className="flex flex-col items-center px-4 py-6 border-2 border-gray-200 rounded-lg">
+            <Wine size={65} className="text-tinta-vino"/>
+            <h2 className="text-3xl font-medium text-gray-900 title-font">{wines.length}</h2>
+            <p className="leading-relaxed">Vinos</p>
+          </div>
+        </Link>
       </section>
-    </>
-  );
+
+    </div>
+  )
 }
 
