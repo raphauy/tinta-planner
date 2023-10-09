6d31972a35b824a7e101adde62d8abcf7e154b8d
raphauy <rapha.uy@rapha.uy>
2023-10-09 08:09:54 -0300

    FechasImportantes - clinete

--- src/app/config/page.tsx
+++ src/app/config/page.tsx
@@ -1,6 +1,13 @@
+import { MoveLeft } from "lucide-react";
 
 export default function ConfigPage() {
   return (
-    <div>ConfigPage</div>
-  )
+    <div className="mt-32">
+      <div className="flex items-center justify-center w-full gap-4 text-2xl">
+          
+          <MoveLeft /> <p>Seleccione algo del menú</p>
+
+      </div>
+    </div>
+)
 }
