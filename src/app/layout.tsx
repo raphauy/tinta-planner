48ff7dd759ba611187e56429222436833b78adba
raphauy <rapha.uy@rapha.uy>
2023-10-27 16:28:30 -0300

    small change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html lang="es">
       <body>
         <AuthContext>
           <ToasterContext />
