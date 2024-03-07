a9a7ca1024b3a8ce1e5966a69cde5aee445e31cc
raphauy <rapha.uy@rapha.uy>
2024-03-07 08:14:02 -0300

    feature: whatsapp messages

--- src/app/whatsapp/messages/message-actions.ts
+++ src/app/whatsapp/messages/message-actions.ts
@@ -1,7 +1,6 @@
 "use server"
   
 import { revalidatePath } from "next/cache"
-import { getCurrentUser } from "@/lib/auth"
 import { MessageDAO, MessageFormValues, createMessage, updateMessage, getFullMessageDAO, deleteMessage } from "@/services/message-services"
 
 
